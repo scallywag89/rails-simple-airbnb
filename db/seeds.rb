44aac6ffab840f0d2982a0d598bdd22651fbe70f
bennettmackenzie <bamsubscriptions@gmail.com>
2022-06-09 21:10:03 +1000

    ran out of time

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-london_flat = Flat.create!(
+Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
@@ -14,7 +14,7 @@ london_flat = Flat.create!(
   number_of_guests: 3
 )
 
-paris_flat = Flat.create!(
+Flat.create!(
   name: 'Beautiful Marais flat',
   address: '8 Rue du Plâtre, 75004 Paris, France',
   description: 'The Hotel du Vieux Marais, once owned by the architect François Mansart, embodies in a most enchanting way the romantic soul and effortless elegance of the Paris of yesteryear.',
@@ -22,7 +22,7 @@ paris_flat = Flat.create!(
   number_of_guests: 4
 )
 
-rome_flat = Flat.create!(
+Flat.create!(
   name: 'Quiet Roman apartment',
   address: 'Via dei Marsi, 42, 00185 Roma RM, Italy',
   description: 'Appartamento Deluxe è la soluzione ideale per famiglie numerose o gruppi di amici che non vogliono rinunciare a nessuna comodità durante il loro soggiorno. ',
@@ -30,7 +30,7 @@ rome_flat = Flat.create!(
   number_of_guests: 6
 )
 
-edinburgh_flat = Flat.create!(
+Flat.create!(
   name: 'Charming Edinburgh flat',
   address: '131-133 High St Edinburgh, Scotland',
   description: 'Large, luxurious flat in old whisky wine warehouse ideally located for easy access to the vibrant Shore area of Leith with its fantastic selection of restaurants and attractions as well as being a short bus ride to the centre of Edinburgh.',
